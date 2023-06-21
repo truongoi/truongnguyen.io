1c909f8430245c45fbc7bb4fd06df0129cbe2480
Truong Nguyen <truongoi@yahoo.com>
2023-06-21 16:18:46 -0700

    Move assets to public folder

--- tina/config.ts
+++ tina/config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   media: {
     tina: {
       publicFolder: "public",
-      mediaRoot: "../images/posts/",
+      mediaRoot: "images/posts",
     },
   },
   schema: {
